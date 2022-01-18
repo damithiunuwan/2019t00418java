5f7fdbccde46046822479fb994ac87eacfaaaba3
damith perera <damithperera777@gmail.com>
2022-01-18 13:13:50 +0530

    added if examples to main

--- vehicle_project/src/com/uoc/Main.java
+++ vehicle_project/src/com/uoc/Main.java
@@ -3,6 +3,11 @@ package com.uoc;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hello");
+
+        int a=10;git 
+        double b=a;
+        System.out.println(b);
+
     }
 }
