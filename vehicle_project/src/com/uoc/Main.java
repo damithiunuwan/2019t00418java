f0628b13aaadd1615e68fae2af50b8085a8eca14
damith perera <damithperera777@gmail.com>
2022-01-18 13:56:30 +0530

    added if examples

--- vehicle_project/src/com/uoc/Main.java
+++ vehicle_project/src/com/uoc/Main.java
@@ -3,11 +3,22 @@ package com.uoc;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
-
-        int a=10;git 
-        double b=a;
-        System.out.println(b);
+        int b=5;
+        switch (b){
+            case 1:
+                System.out.println("google");
+                System.out.println("samsung");
+                break;
+            case 2:
+                System.out.println("huawei");
+                System.out.println("nokia");
+                break;
+            case 3:
+                System.out.println("vendor");
+                break;
+            default:
+                System.out.println("unknown");
+        }
 
     }
 }
